4430eca995ada30b22849fe38bba36822e849c1e
Arlen <arlenbajgazakov@gmail.com>
2025-07-22 00:34:39 +0600

    homework

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,28 @@ from django.db import models
 "SELECT 1 FROM posts WHERE id=1;--> Post.object.get(id=1)"
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
+    image = models.ImageField(null=True,blank=True)
     title = models.CharField(max_length=256)
     content = models.CharField(max_length=456)
     rate = models.IntegerField(default=0,null=True)
-
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
 
     def __str__(self):
         return f"{self.title}-{self.content}"
