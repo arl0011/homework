bb1fbced3f794df5660f3ae2ffc285e25c81c89c
Arlen <arlenbajgazakov@gmail.com>
2025-07-28 18:23:20 +0600

    lesson5

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse,redirect
 import random
 from posts.models import Post
 from posts.forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -11,20 +12,20 @@ def home(request):
         return HttpResponse("Hello world mf")
 
 
-
+@login_required(login_url="/login")
 
 def post_list_view(request):
     if request.method == "GET":
         posts = Post.objects.all()
         return render(request,"posts/post_list.html",context={"posts":posts})
 
-
+@login_required(login_url="/login")
 def post_detail_view(request,post_id):
     if request.method =="GET":
         post = Post.objects.filter(id=post_id).first()
         return render(request,"posts/post_detail.html",context={"post":post})
 
-
+@login_required(login_url="/login")
 def post_create_view(request):
     if request.method =="GET":
         form = PostForm()
